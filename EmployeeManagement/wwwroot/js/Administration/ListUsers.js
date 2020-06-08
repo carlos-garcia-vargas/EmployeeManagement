d7d1a02c81ebf289ecee13704d99262df67aaba0
Carlos Garcia <cgarcia@prokarma.com>
2020-06-08 16:59:35 -0600

    Add project files.

---
+++ EmployeeManagement/wwwroot/js/Administration/ListUsers.js
@@ -0,0 +1,33 @@
+﻿$('#confirmDeleteModal').on('show.bs.modal', function (e) {
+    var deleteurl = $(e.relatedTarget).data('url');
+    $('#confirmDeleteModalForm').attr('action', deleteurl);
+});
+
+$('#confirmUnlockModal').on('show.bs.modal', function (e) {
+    var confirmUser = $(e.relatedTarget);
+    $('#confirmUnlockButton').data('confirm-url', confirmUser.data('url'));
+    $('#confirmUnlockButton').data('user-id', confirmUser.data('userid'));
+});
+
+$('#confirmUnlockButton').on('click', function () {
+    var url = $(this).data('confirm-url');
+    var userId = $(this).data('user-id');
+    $.post(url, { userId },  function (data) {
+        console.log("success");
+        if (data.errorMessage === "OK") {
+            alert("Success");
+            $.get("/Administration/ListUsers", function (dataListUsers) {
+                $('#list_users_table').html(dataListUsers.stringView);
+            });
+        } else {
+            alert(data.errorMessage);
+        }
+
+    }).fail(function (e) {
+        alert("error");
+        console.log(e);
+    }).always(function () {
+        console.log("finished");
+        $('#confirmUnlockModal').modal('hide');
+    });
+});
